b931f0bfb5efda82aba6d6c0212bf2edadc855c8
kso1204 <kso12041204@naver.com>
2024-10-14 01:47:10 +0000

    encoding utf-8

--- restock.py
+++ restock.py
@@ -10,7 +10,9 @@ import logging
 logging.basicConfig(filename='restock_monitoring.log',
                     level=logging.INFO,
                     format='%(asctime)s - %(message)s', 
-                    datefmt='%Y-%m-%d %H:%M:%S')
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    encoding="utf-8"
+                    )
 logging.info("재고 모니터링 시작")
 
 load_dotenv()
