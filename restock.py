d8b38cfac5dede918ca4a57ac8f3d126ff46f49a
sangwookkim <sangwook_kim@tmax.co.kr>
2024-10-14 10:29:15 +0900

    베이비세트 화이트 추가

--- restock.py
+++ restock.py
@@ -12,6 +12,10 @@ logging.info("재고 모니터링 시작")
 
 load_dotenv()
 
+
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
 # Webhook URL을 여기에 입력합니다.
 webhook_url = os.getenv('SLACK_WEBHOOK_URL')
 
@@ -43,15 +47,26 @@ def check_stock():
         
         # "화이트워시(S)" 옵션 찾기
         options_div = soup.find('div', class_=['item_add_option_box'])
-        select = options_div.find('select', class_='chosen-select')
-        options = select.find_all('option')
-        
-        # 각 옵션을 순회하며 품절 여부를 확인
-        for option in options:
-            option_text = option.text.strip()
-            if '화이트워시(S)' in option_text and 'disabled' not in option.attrs:
-                print("화이트워시(S) is available.")
-                return True
+        if options_div:
+            select = options_div.find('select', class_='chosen-select')
+            if select:
+                options = select.find_all('option')
+                
+                # 모든 option 태그를 찾아서 각각의 텍스트와 값을 출력
+                for index, option in enumerate(options):
+                    value = option.get('value')  # option의 value 속성
+                    text = option.get_text(strip=True)  # option의 텍스트, 공백 제거
+                    logging.info(f"Option {index}: Text: {text}, Value: {value}")
+                    option_text = option.text.strip()
+                    if '화이트워시(S)' in option_text and 'disabled' not in option.attrs:
+                        print("화이트워시(S) is available.")
+                        return True
+                    else:
+                        print("화이트워시(S) is not available.")
+            else:
+                print("Select element with class 'chosen-select' not found.")
+        else:
+            print("Div with class 'item_add_option_box' not found.")
             
     return False  # 품절 상태
 
@@ -80,6 +95,40 @@ def check_stock2():
         
     return False  # 품절 상태
 
+# http://fnnk.co.kr/goods/goods_view.php?goodsNo=1000000795
+# 스토케 트립트랩 베이비세트
+def check_stock3():
+    url = 'http://fnnk.co.kr/goods/goods_view.php?goodsNo=1000000795'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # "화이트워시(S)" 옵션 찾기
+        options_div = soup.find('div', class_=['item_add_option_box'])
+        if options_div:
+            select = options_div.find('select', class_='chosen-select')
+            if select:
+                options = select.find_all('option')
+                
+                # 모든 option 태그를 찾아서 각각의 텍스트와 값을 출력
+                for index, option in enumerate(options):
+                    value = option.get('value')  # option의 value 속성
+                    text = option.get_text(strip=True)  # option의 텍스트, 공백 제거
+                    logging.info(f"Option {index}: Text: {text}, Value: {value}")
+                    option_text = option.text.strip()
+                    if '화이트(S)' in option_text and 'disabled' not in option.attrs:
+                        print("화이트(S) is available.")
+                        return True
+                    else:
+                        print("화이트(S) is not available.")
+            else:
+                print("Select element with class 'chosen-select' not found.")
+        else:
+            print("Div with class 'item_add_option_box' not found.")
+            
+    return False  # 품절 상태
+
 def monitor_stock1():
     while True:
         logging.info("화이트워시 재고 확인 중")
@@ -91,6 +140,10 @@ def monitor_stock1():
             print("화이트워시(S) 재고 없음, 다시 확인 중...")
         time.sleep(60)  # 60초 후 다시 확인
 
+        # 3시간마다 슬랙 메시지 전송
+        send_slack_message("화이트워시(S) 모니터링 중입니다. 아직 재고가 없습니다.")
+        time.sleep(10800)  # 3시간 대기
+
 def monitor_stock2():
     while True:
         logging.info("뉴본 재고 확인중")
@@ -101,6 +154,26 @@ def monitor_stock2():
         else:
             print("뉴본 신생아 세트 재고 없음, 다시 확인 중...")
         time.sleep(60)  # 60초 후 다시 확인
+        
+        # 3시간마다 슬랙 메시지 전송
+        send_slack_message("뉴본 신생아 세트 모니터링 중입니다. 아직 재고가 없습니다.")
+        time.sleep(10800)  # 3시간 대기
+
+
+def monitor_stock3():
+    while True:
+        logging.info("트립트랩 베이비세트 확인중")
+        in_stock = check_stock3()
+        if in_stock:
+            send_slack_message("트립트랩 베이비세트 재고가 다시 들어왔습니다!")
+            break  # 조건에 맞으면 루프 종료
+        else:
+            print("트립트랩 베이비세트 세트 재고 없음, 다시 확인 중...")
+        time.sleep(60)  # 60초 후 다시 확인
+        
+        # 3시간마다 슬랙 메시지 전송
+        send_slack_message("트립트랩 베이비세트 모니터링 중입니다. 아직 재고가 없습니다.")
+        time.sleep(10800)  # 3시간 대기
 
 # 실행
 if __name__ == '__main__':
@@ -108,12 +181,15 @@ if __name__ == '__main__':
     # 각 모니터링 함수를 스레드로 실행
     thread1 = threading.Thread(target=monitor_stock1)
     thread2 = threading.Thread(target=monitor_stock2)
+    thread3 = threading.Thread(target=monitor_stock3)
 
     thread1.start()  # 화이트워시(S) 재고 확인 시작
     thread2.start()  # 뉴본 신생아 세트 재고 확인 시작
+    thread3.start()  # 트립트랩 베이비세트 재고 확인 시작
 
     thread1.join()  # 스레드1 종료 대기
     thread2.join()  # 스레드2 종료 대기
+    thread3.join()  # 스레드3 종료 대기
     
     
-    print("모든 재고 확인이 완료되었습니다.")  # 두 스레드가 종료된 후 메시지 출력
+    print("모든 재고 확인이 완료되었습니다.")  # 스레드가 종료된 후 메시지 출력
